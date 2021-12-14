096edbf999b2ac261d8e1423ef30d48d66ad57de
nuaawangrong <sszwangrong@126.com>
2021-12-15 00:31:42 +0800

    2-7 新增hello接口测试test.http

--- src/main/java/com/wangrong/wiki/controller/TestController.java
+++ src/main/java/com/wangrong/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.wangrong.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,11 @@ public class TestController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post ," + name;
     }
 }
