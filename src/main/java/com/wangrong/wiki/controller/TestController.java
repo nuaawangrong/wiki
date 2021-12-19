693e336614d24540dd8d82469347c28e4ee28ce1
nuaawangrong <sszwangrong@126.com>
2021-12-19 18:02:11 +0800

    3-5 增加demo表列表查询

--- src/main/java/com/wangrong/wiki/controller/TestController.java
+++ src/main/java/com/wangrong/wiki/controller/TestController.java
@@ -34,4 +34,6 @@ public class TestController {
     public List<Test> list() {
         return testService.list();
     }
+
+
 }
---
+++ src/main/java/com/wangrong/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.wangrong.wiki.service;
+
+import com.wangrong.wiki.domain.Demo;
+import com.wangrong.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
