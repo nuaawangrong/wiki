af5146ff34bb3f23ed2e4fe505aecb88dc77e33b
nuaawangrong <sszwangrong@126.com>
2021-12-19 17:23:00 +0800

    3-5 将数据库改为wikidev

--- doc/all.sql
+++ doc/all.sql
@@ -4,6 +4,6 @@ create table `test` (
                         `name` varchar(50) comment '名称',
                         `password` varchar(50) comment '密码',
                         primary key (`id`)
-)engine=innodb default char set=utf8mb4 comment='测试'
+)engine=innodb default char set=utf8mb4 comment='测试';
 
 INSERT INTO `test` (id,name,password) VALUES(1,'测试','password');
