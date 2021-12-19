55e7febd59a1c947480ce6f60504c6514d290578
nuaawangrong <sszwangrong@126.com>
2021-12-19 17:07:49 +0800

    3-4 mybatis使用示例:开发/test/list接口，查询test表所有数据

--- doc/all.sql
+++ doc/all.sql
@@ -5,3 +5,5 @@ create table `test` (
                         `password` varchar(50) comment '密码',
                         primary key (`id`)
 )engine=innodb default char set=utf8mb4 comment='测试'
+
+INSERT INTO `test` (id,name,password) VALUES(1,'测试','password');
---
+++ src/main/java/com/wangrong/wiki/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.wangrong.wiki.mapper;
+
+import com.wangrong.wiki.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    public List<Test> list();
+
+}
