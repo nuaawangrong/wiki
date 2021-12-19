e12eed7e1021bc3f6f66ecc42a13dca546ffb696
nuaawangrong <sszwangrong@126.com>
2021-12-19 16:13:59 +0800

    3-3 增加all.sql脚本

---
+++ doc/all.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `test`;
+create table `test` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '名称',
+                        `password` varchar(50) comment '密码',
+                        primary key (`id`)
+)engine=innodb default char set=utf8mb4 comment='测试'
