8f2c2145efd3afe383020e614e9f6188b33f4b92
nuaawangrong <sszwangrong@126.com>
2021-12-21 11:07:22 +0800

    6-6 电子书管理页面前后端分页整合完成

--- web/src/views/admin/Admin-ebook.vue
+++ web/src/views/admin/Admin-ebook.vue
@@ -10,7 +10,7 @@
           :data-source="ebooks"
           :pagination="pagination"
           :loading="loading"
-          @change="handleTablChange"
+          @change="handleTableChange"
       >
         <template #cover="{ text: cover }">
           <img v-if="cover" :src="cover" alt="avatar" />
@@ -43,7 +43,7 @@ export default defineComponent({
     const ebooks = ref();
     const pagination = ref({
       current: 1,
-      pageSize: 3,
+      pageSize: 2,
       total: 0
     });
     const loading = ref(false);
@@ -91,14 +91,19 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
-      axios.get("/ebook/list", params).then((response) => {
+      axios.get("/ebook/list",{
+        params: {
+          page : params.page,
+          size : params.size
+        }
+      }).then((response) => {
         loading.value = false;
         const data = response.data;
-        ebooks.value = data.content;
+        ebooks.value = data.content.list;
 
         // 重置分页按钮
         pagination.value.current = params.page;
-        // pagination.value.total = data.content.total;
+        pagination.value.total = data.content.total;
       });
     };
 
@@ -114,7 +119,10 @@ export default defineComponent({
     };
 
     onMounted(() => {
-      handleQuery({});
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize,
+      });
     });
 
     return {
