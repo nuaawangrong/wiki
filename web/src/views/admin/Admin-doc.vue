39795daadda1be1b976684dfde6250de80aca2f2
nuaawangrong <sszwangrong@126.com>
2021-12-28 11:35:54 +0800

    8-9 文档管理布局美化 表格紧凑 按钮变小 表单变大 页面打开时就初始化富文本框，z-index变小，试下拉选择框可以显示在富文本框前 左边的表格默认全部显示

--- web/src/views/admin/Admin-doc.vue
+++ web/src/views/admin/Admin-doc.vue
@@ -3,7 +3,7 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-row>
+      <a-row :gutter="24">
         <a-col :span="6">
           <p>
             <a-form layout="inline" :model="param">
@@ -21,24 +21,26 @@
                   新增
                 </a-button>
               </a-form-item>
-
             </a-form>
           </p>
 
           <a-table
+              v-if="level1 && level1.length>0"
               :columns="columns"
               :row-key="record => record.id"
               :data-source="level1"
               :loading="loading"
               :pagination="false"
-              @change="handleTableChange"
+              defaultExpandAllRows="true"
+              size="small"
           >
-            <template #cover="{ text: cover }">
-              <img v-if="cover" :src="cover" alt="avatar" />
+
+            <template #name="{ text, record }">
+              {{record.sort}} {{text}}
             </template>
             <template v-slot:action="{ text, record }">
               <a-space size="small">
-                <a-button type="primary" @click="edit(record)">
+                <a-button type="primary" @click="edit(record)" size="small">
                   编辑
                 </a-button>
 
@@ -48,7 +50,7 @@
                     cancel-text="否"
                     @confirm="handleDelete(record.id)"
                 >
-                  <a-button type="danger" >
+                  <a-button type="danger" size="small">
                     刪除
                   </a-button>
                 </a-popconfirm>
@@ -57,11 +59,20 @@
           </a-table>
         </a-col>
         <a-col :span="16">
-          <a-form :model="doc" :label-col="{span: 6}" :wrapper-col="{ span: 18 }">
-            <a-form-item label="名称">
-              <a-input v-model:value="doc.name" />
+          <p>
+            <a-form layout="inline" :model="param">
+              <a-form-item>
+                <a-button type="primary" @click="handleSave()">
+                  保存
+                </a-button>
+              </a-form-item>
+            </a-form>
+          </p>
+          <a-form :model="doc" layout="vertical">
+            <a-form-item>
+              <a-input v-model:value="doc.name" placeholder="名称"/>
             </a-form-item>
-            <a-form-item label="父文档 树形">
+            <a-form-item>
               <a-tree-select
                   v-model:value="doc.parent"
                   style="width: 100%"
@@ -73,10 +84,10 @@
               >
               </a-tree-select>
             </a-form-item>
-            <a-form-item label="顺序">
-              <a-input v-model:value="doc.sort" />
+            <a-form-item>
+              <a-input v-model:value="doc.sort" placeholder="顺序"/>
             </a-form-item>
-            <a-form-item label="内容">
+            <a-form-item>
               <div id="content"></div>
             </a-form-item>
           </a-form>
@@ -120,16 +131,8 @@ export default defineComponent({
     const columns = [
       {
         title: '名称',
-        dataIndex: 'name'
-      },
-      {
-        title: '父文档',
-        key: 'parent',
-        dataIndex: 'parent'
-      },
-      {
-        title: '顺序',
-        dataIndex: 'sort'
+        dataIndex: 'name',
+        slots: {customRender: 'name'}
       },
       {
         title: 'Action',
@@ -162,7 +165,11 @@ export default defineComponent({
           docs.value = data.content;
 
           level1.value = [];
-          level1.value = Tool.array2Tree(docs.value,0);
+
+          setTimeout(function () {
+            level1.value = Tool.array2Tree(docs.value,0);
+          },100)
+
         } else {
           message.error(data.message);
         }
@@ -180,8 +187,9 @@ export default defineComponent({
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const editor = new E('#content');
+    editor.config.zIndex = 0;
 
-    const handleModalOk = () => {
+    const handleSave = () => {
       modalLoading.value = true;
       axios.post("/doc/save",doc.value).then((response) => {
         modalLoading.value = false;
@@ -270,10 +278,6 @@ export default defineComponent({
       setDisable(treeSelectData.value, record.id);
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-
-      setTimeout(function () {
-        editor.create();
-      },100);
     }
 
     /**
@@ -287,10 +291,6 @@ export default defineComponent({
       treeSelectData.value = Tool.copy(level1.value);
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-
-      setTimeout(function () {
-        editor.create();
-      },100);
     }
 
     /**
@@ -310,6 +310,7 @@ export default defineComponent({
 
     onMounted(() => {
       handleQuery();
+      editor.create();
     });
 
     return {
@@ -328,7 +329,7 @@ export default defineComponent({
       doc,
       modalVisible,
       modalLoading,
-      handleModalOk,
+      handleSave,
     }
   }
 });
