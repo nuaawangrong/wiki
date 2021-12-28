87a95decf111ddc6879376360a6f3fd54482e291
nuaawangrong <sszwangrong@126.com>
2021-12-28 21:58:51 +0800

    8-12  点击文档节点时，加载并显示文档内容

--- web/src/views/Doc.vue
+++ web/src/views/Doc.vue
@@ -13,6 +13,7 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div :innerHTML="html"></div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -33,6 +34,7 @@ export default defineComponent({
   setup() {
     const route = useRoute();
     const docs = ref();
+    const html = ref();
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -64,14 +66,42 @@ export default defineComponent({
         }
       });
     };
+
+
+    /**
+     * 内容查询
+     **/
+    const handleQueryContent = (id: number) => {
+      axios.get("/doc/find-content/" + id).then((response) => {
+        const data = response.data;
+        if(data.success) {
+          html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const onSelect = (selectedKeys: any, info: any) => {
+      // console.log("selected:", selectedKeys);
+      // console.log("info:", info)
+      if(Tool.isNotEmpty(selectedKeys)) {
+        handleQueryContent(selectedKeys[0]);
+      }
+    }
+
     onMounted(() => {
       handleQuery();
     });
 
     return {
       level1,
+      html,
 
       handleQuery,
+      handleQueryContent,
+      onSelect,
+
       }
   }
 });
