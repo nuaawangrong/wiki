a1c4ba2cf36505584c0762e08bf588ac0bcc8033
nuaawangrong <sszwangrong@126.com>
2021-12-30 20:47:41 +0800

    9-9 集成redis,登录成功后将登录信息放入到redis中

--- src/main/java/com/wangrong/wiki/controller/UserController.java
+++ src/main/java/com/wangrong/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.wangrong.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wangrong.wiki.req.UserLoginReq;
 import com.wangrong.wiki.req.UserQueryReq;
 import com.wangrong.wiki.req.UserResetPasswordReq;
@@ -9,18 +10,31 @@ import com.wangrong.wiki.resp.PageResp;
 import com.wangrong.wiki.resp.UserLoginResp;
 import com.wangrong.wiki.resp.UserQueryResp;
 import com.wangrong.wiki.service.UserService;
+import com.wangrong.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(("/user"))
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+    
     @Resource
     private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private SnowFlake snowFlake;
 
 
     @GetMapping("/list")
@@ -63,15 +77,15 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token:{},并放入redis中",token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600*24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
 
 
 }
-
-
-
-
-
-
