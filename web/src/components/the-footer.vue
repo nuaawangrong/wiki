c17bec59fd6574320c4456a1124ba968ffafb264
nuaawangrong <sszwangrong@126.com>
2021-12-30 21:24:03 +0800

    9-9 使用vuex保存登录后的用户信息，方便其他组件获取信息。 遗留问题:刷新后值就没有了。

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,14 +1,22 @@
 <template>
   <a-layout-footer style="text-align: center">
-    Spring Boot + Vue3 构建Wiki知识管理系统
+    Spring Boot + Vue3 构建Wiki知识管理系统, 欢迎：{{user.name}}
   </a-layout-footer>
 </template>
 
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent, computed} from 'vue';
+import store from "@/store";
 
 export default defineComponent({
   name: 'the-footer',
+  setup () {
+    const user = computed(() => store.state.user)
+
+    return {
+      user,
+    }
+  }
 });
 </script>
--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,12 +1,20 @@
 import { createStore } from 'vuex'
 
-export default createStore({
+const store = createStore({
   state: {
+    user: {}
   },
   mutations: {
+    // 对变量的操作，同步
+    setUser (state, user) {
+      state.user = user;
+    }
   },
   actions: {
+    // 对变量的操作，异步
   },
   modules: {
   }
 })
+
+export default store;
