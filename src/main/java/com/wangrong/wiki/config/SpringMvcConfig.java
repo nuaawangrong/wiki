8df9a35c8ec12d7a6fc2c6fca60476a897d53730
nuaawangrong <sszwangrong@126.com>
2022-01-01 16:20:35 +0800

    9-11 前后端增加登录拦截 修复Bug：前后端存储token统一使用String类型

--- src/main/java/com/wangrong/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/wangrong/wiki/config/SpringMvcConfig.java
@@ -1,40 +1,42 @@
-//package com.wangrong.wiki.config;
-//
-//import com.wangrong.wiki.interceptor.LoginInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
+package com.wangrong.wiki.config;
+
+import com.wangrong.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
 //    @Resource
-//    LoginInterceptor loginInterceptor;
-//
-////    @Resource
-////    ActionInterceptor actionInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(loginInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-////                .excludePathPatterns(
-////                        "/test/**",
-////                        "/redis/**",
-////                        "/user/login",
-////                        "/category/all",
-////                        "/ebook/list",
-////                        "/doc/all/**",
-////                        "/doc/vote/**",
-////                        "/doc/find-content/**",
-////                        "/ebook-snapshot/**"
-////                );
-//
-////        registry.addInterceptor(actionInterceptor)
-////                .addPathPatterns(
-////                        "/*/save",
-////                        "/*/delete/**",
-////                        "/*/reset-password");
-//    }
-//}
+//    ActionInterceptor actionInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+//                .excludePathPatterns("/login");
+                .excludePathPatterns(
+//                        "/user/logout/**",
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+//                        "/doc/vote/**",
+                        "/doc/find-content/**"
+//                        "/ebook-snapshot/**"
+                );
+
+//        registry.addInterceptor(actionInterceptor)
+//                .addPathPatterns(
+//                        "/*/save",
+//                        "/*/delete/**",
+//                        "/*/reset-password");
+    }
+}
