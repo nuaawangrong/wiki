6a832b6dda130f21d7a9aef7fe07c51d54447854
nuaawangrong <sszwangrong@126.com>
2022-01-01 23:19:31 +0800

    10-2 文档页面显示文档阅读数点赞数

--- web/src/views/Doc.vue
+++ web/src/views/Doc.vue
@@ -15,6 +15,14 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div>
+            <h2>{{doc.name}}</h2>
+            <div>
+              <span>阅读数: {{doc.viewCount}}</span> &nbsp; &nbsp;
+              <span>阅读数: {{doc.voteCount}}</span>
+            </div>
+            <a-divider style="height: 2px; background-color: #9999cc" />
+          </div>
           <div class="wangeditor" :innerHTML="html"></div>
         </a-col>
       </a-row>
@@ -39,6 +47,9 @@ export default defineComponent({
     const html = ref();
     const defaultSelectedKeys = ref();
     defaultSelectedKeys.value = [];
+    //当前选中的文档
+    const doc = ref();
+    doc.value = {};
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -84,6 +95,8 @@ export default defineComponent({
           if(Tool.isNotEmpty(level1)) {
             defaultSelectedKeys.value = [level1.value[0].id];
             handleQueryContent(level1.value[0].id);
+            //初始显示文档信息
+            doc.value = level1.value[0];
           }
         } else {
           message.error(data.message);
@@ -92,10 +105,17 @@ export default defineComponent({
     };
 
     const onSelect = (selectedKeys: any, info: any) => {
-      // console.log("selected:", selectedKeys);
-      // console.log("info:", info)
+      console.log("selected:", selectedKeys);
+      console.log("info:", info)
       if(Tool.isNotEmpty(selectedKeys)) {
+        //选中某一节点时，加载该节点的文档信息
+        console.log("info.selectedNodes[0]: ", info.selectedNodes[0]);
+        console.log("info.selectedNodes[0].props : ", info.selectedNodes[0].props);
+        doc.value = info.selectedNodes[0];
+
+        //加载内容
         handleQueryContent(selectedKeys[0]);
+
       }
     }
 
@@ -110,6 +130,7 @@ export default defineComponent({
       handleQuery,
       handleQueryContent,
       onSelect,
+      doc,
 
       }
   }
