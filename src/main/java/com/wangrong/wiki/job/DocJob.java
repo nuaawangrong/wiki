d1ae2717de68441937ed8b164d7eddd06daff762
nuaawangrong <sszwangrong@126.com>
2022-01-02 22:15:51 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL,增加日志

--- src/main/java/com/wangrong/wiki/job/DocJob.java
+++ src/main/java/com/wangrong/wiki/job/DocJob.java
@@ -19,9 +19,12 @@ import javax.annotation.Resource;
     /**
      * 每30秒更新电子书信息
      */
-    @Scheduled(cron = "5/35 * * * * ?")
+    @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
+        LOG.info("更新电子书下的文档数据开始：");
+        long start = System.currentTimeMillis();
         docService.updateEbookInfo();
+        LOG.info("更新电子书下的文档数据结束，耗时：{}毫秒", System.currentTimeMillis() - start);
     }
 
  }
