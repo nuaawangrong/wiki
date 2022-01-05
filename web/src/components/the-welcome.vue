9bfbc4d3886609cc1b20bdd732fd0797472b6df6
nuaawangrong <sszwangrong@126.com>
2022-01-05 10:32:59 +0800

    11-7 将欢迎文字提取成组件

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>欢迎来到wiki知识库111</h1>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-welcome',
+  setup() {
+
+    return {
+
+    }
+
+  }
+});
+</script>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -24,7 +24,7 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <div class="welcome" v-show="isShowWelcome">
-        <h1>欢迎来到wiki知识库</h1>
+        <TheWelcome></TheWelcome>
       </div>
       <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 16, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
@@ -64,9 +64,14 @@ import { defineComponent, onMounted, ref, reactive,toRef  } from 'vue';
 import axios from 'axios';
 import { message } from "ant-design-vue";
 import {Tool} from "@/util/tool"
+import TheWelcome from '@/components/the-welcome.vue';
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    TheWelcome,
+  },
+
   setup() {
     const ebooks = ref();
 
